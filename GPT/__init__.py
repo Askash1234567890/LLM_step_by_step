6280e1b1b16380ce6ca856a6bed4c91cb0d02f85
askash <askash@MacBook-Air-Alex.local>
2026-05-26 13:51:47 +0300

    add top_k and top_p in self.generate

--- GPT/__init__.py
+++ GPT/__init__.py
@@ -57,20 +57,51 @@ class GPT(torch.nn.Module):
         return self.linear(output)
 
     def generate(
-        self, x: torch.Tensor, 
-        max_new_tokens: int,
-        do_sample: bool = False,
-        temperature: float = 1.0
-    ):
-        for _ in range(max_new_tokens):
-            res = x[:, -self.max_seq_len:]
-            logits = self(res) / temperature
-            probs = torch.softmax(logits, dim=-1)[:, -1, :]
-            if do_sample:
-                next_token = torch.multinomial(probs, num_samples=1)
-            else:
-                next_token = torch.unsqueeze(probs.argmax(dim=-1), 1)
-            x = torch.cat((x, next_token), dim=1)
+            self,
+            x: torch.Tensor,
+            max_new_tokens: int,
+            do_sample=True,
+            temperature: float = 1.0,
+            top_p: float = None,
+            top_k: int = None
+        ):
+        self.eval()
+        with torch.no_grad():
+            for _ in range(max_new_tokens):
+                input_x = x[:, -self.max_seq_len:]
+                logits = (self(input_x) / temperature)[:, -1, :]
+                # logits.shape = (batch_size, vocab_size)
+
+                if do_sample:
+                    if top_k is not None:
+                        top_k_clamped = min(top_k, logits.size(-1))
+                        # treshold findind — the smallest from top_k logits
+                        threshold = torch.topk(logits, top_k_clamped, dim=-1).values[..., -1, None]
+                        logits = logits.masked_fill(logits < threshold, float('-inf'))
+
+                    if top_p is not None:
+                        probs = torch.softmax(logits, dim=-1)
+                        sorted_probs, sorted_indices = torch.sort(probs, descending=True, dim=-1)
+                        cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+                        # del tokens when cumsum > top_p
+                        sorted_indices_to_remove = cumulative_probs > top_p
+                        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+                        sorted_indices_to_remove[..., 0] = 0
+
+                        # reorganize mask
+                        indices_to_remove = torch.zeros_like(logits, dtype=torch.bool)
+                        indices_to_remove.scatter_(-1, sorted_indices, sorted_indices_to_remove)
+                        logits = logits.masked_fill(indices_to_remove, float('-inf'))
+
+                probs = torch.softmax(logits, dim=-1)
+
+                if do_sample:
+                    new_token = torch.multinomial(probs, num_samples=1)
+                else:
+                    new_token = torch.unsqueeze(probs.argmax(dim=-1), 1)
+
+                x = torch.cat((x, new_token), dim=1)
         return x
 
     def save(self, path):
